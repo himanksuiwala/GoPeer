fc8d34f9de02e79937167cd61154455b8d6a3890
Himank Suiwala <himanksuiwala@gmail.com>
2024-07-31 01:18:49 +0530

    Started listening server and handling multiple connections

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 )
 
+const PORT string = ":3000"
+
 func main() {
-	createFolder()
-	createFile()
-	readFile()
+	// createFolder()
+	// createFile()
+	// readFile()
 	// deleteFile()
+
+	runServer()
 }
 
 func createFolder() {
@@ -36,4 +41,40 @@ func readFile() {
 		fmt.Printf("%s\n", err)
 	}
 	fmt.Println("[File]:", string(fileContents))
+}
+
+func runServer() {
+	ln, err := net.Listen("tcp", PORT)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+
+	defer ln.Close()
+
+	fmt.Printf("Server started on Port %s\n", PORT)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			continue
+		}
+
+		fmt.Printf("[Server@%s]: %s\n", PORT, conn.RemoteAddr().String())
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	// defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	fmt.Printf("[Server@%s]: %s\n", PORT, string(buf[:n]))
 }
